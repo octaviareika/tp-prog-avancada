44d1a412d49ecf35f259e75553a83db3dd1237c4
Cecilia <ceciliaperet81@gmail.com>
2026-05-10 15:22:22 -0300

    update: implementando funcoes

--- main.cpp
+++ main.cpp
@@ -22,3 +22,32 @@ typedef struct No {
     Point p;
     int id;
 } No, *ptrNo;
+
+
+double dist(Point a, Point b){
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+double orientation(Point a, Point b, Point c){
+    double val = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y);
+    if(fabs(val) < EPSILON) return 0; // considere que ele eh tao proximo de 0 que eh 0
+    return (val > 0) ? 1 : -1;
+}
+
+bool on_segment(Point p, Point q, Point r){
+    return (q.x <= fmax(p.x, r.x) && q.x >= fmin(p.x, r.x) &&
+            q.y <= fmax(p.y, r.y) && q.y >= fmin(p.y, r.y));
+}
+
+
+bool segments_intersect(Segment p1, Segment q1){
+    double o1 = orientation(p1.a, p1.b, q1.a);
+    double o2 = orientation(p1.a, p1.b, q1.b);
+    double o3 = orientation(q1.a, q1.b, p1.a);
+    double o4 = orientation(q1.a, q1.b, p1.b);
+    
+    if (o1 != o2 && o3 != o4) return true;
+    
+    return false;
+    
+}
